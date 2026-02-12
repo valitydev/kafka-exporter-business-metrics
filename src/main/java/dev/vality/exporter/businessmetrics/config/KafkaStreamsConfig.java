648b2c630e313290f08c1269afbd923207ec40fb
Egor Cherniak <cherniak3@yandex.ru>
2026-02-12 17:34:59 +0300

    Fix exporter kafka streams configuration (#6)
    * add logs + fix kafka streams configuration
    
    * Try to fix streams config
    
    * cleanup

--- src/main/java/dev/vality/exporter/businessmetrics/config/KafkaStreamsConfig.java
+++ src/main/java/dev/vality/exporter/businessmetrics/config/KafkaStreamsConfig.java
@@ -2,7 +2,10 @@ package dev.vality.exporter.businessmetrics.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -17,18 +20,27 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaStreamsConfig {
 
+    private final KafkaProperties kafkaProperties;
+    private final ObjectProvider<SslBundles> sslBundles;
+
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    public KafkaStreamsConfiguration streamsConfig(
-            KafkaProperties kafkaProperties
-    ) {
-        Map<String, Object> props = new HashMap<>(kafkaProperties.getStreams().getProperties());
+    public KafkaStreamsConfiguration streamsConfig() {
+        Map<String, Object> props = kafkaProperties.buildStreamsProperties(sslBundles.getIfAvailable());
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, kafkaProperties.getStreams().getApplicationId());
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, kafkaProperties.getClientId());
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
                 kafkaProperties.getBootstrapServers());
         props.putIfAbsent(StreamsConfig.PROCESSING_GUARANTEE_CONFIG,
                 StreamsConfig.EXACTLY_ONCE_V2);
         props.putIfAbsent(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 2);
-
+        addDefaultProperties(props);
         return new KafkaStreamsConfiguration(props);
+    }
+
+    private void addDefaultProperties(Map<String, Object> props) {
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
+        props.put(StreamsConfig.DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
+                LogAndContinueExceptionHandler.class);
     }
 }
